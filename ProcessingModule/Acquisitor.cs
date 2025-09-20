47075bebb00420487da45c1c7a64c6dd045b354b
Milos <Milos@DESKTOP-GL6GJFE>
2025-09-20 16:06:05 +0200

    Acquisitor-proverava da li je vreme za ocitavanje

--- ProcessingModule/Acquisitor.cs
+++ ProcessingModule/Acquisitor.cs
@@ -8,63 +8,78 @@ namespace ProcessingModule
     /// Class containing logic for periodic polling.
     /// </summary>
     public class Acquisitor : IDisposable
-	{
-		private AutoResetEvent acquisitionTrigger;
+    {
+        private AutoResetEvent acquisitionTrigger;
         private IProcessingManager processingManager;
         private Thread acquisitionWorker;
-		private IStateUpdater stateUpdater;
-		private IConfiguration configuration;
+        private IStateUpdater stateUpdater;
+        private IConfiguration configuration;
+        // IZMENA: Dodajemo flag za graceful shutdown
+        private bool disposed = false;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Acquisitor"/> class.
-        /// </summary>
-        /// <param name="acquisitionTrigger">The acquisition trigger.</param>
-        /// <param name="processingManager">The processing manager.</param>
-        /// <param name="stateUpdater">The state updater.</param>
-        /// <param name="configuration">The configuration.</param>
-		public Acquisitor(AutoResetEvent acquisitionTrigger, IProcessingManager processingManager, IStateUpdater stateUpdater, IConfiguration configuration)
-		{
-			this.stateUpdater = stateUpdater;
-			this.acquisitionTrigger = acquisitionTrigger;
-			this.processingManager = processingManager;
-			this.configuration = configuration;
-			this.InitializeAcquisitionThread();
-			this.StartAcquisitionThread();
-		}
+        public Acquisitor(AutoResetEvent acquisitionTrigger, IProcessingManager processingManager, IStateUpdater stateUpdater, IConfiguration configuration)
+        {
+            this.stateUpdater = stateUpdater;
+            this.acquisitionTrigger = acquisitionTrigger;
+            this.processingManager = processingManager;
+            this.configuration = configuration;
+            this.InitializeAcquisitionThread();
+            this.StartAcquisitionThread();
+        }
 
-		#region Private Methods
+        #region Private Methods
 
-        /// <summary>
-        /// Initializes the acquisition thread.
-        /// </summary>
-		private void InitializeAcquisitionThread()
-		{
-			this.acquisitionWorker = new Thread(Acquisition_DoWork);
-			this.acquisitionWorker.Name = "Acquisition thread";
-		}
+        private void InitializeAcquisitionThread()
+        {
+            this.acquisitionWorker = new Thread(Acquisition_DoWork);
+            this.acquisitionWorker.Name = "Acquisition thread";
+        }
 
-        /// <summary>
-        /// Starts the acquisition thread.
-        /// </summary>
-		private void StartAcquisitionThread()
-		{
-			acquisitionWorker.Start();
-		}
+        private void StartAcquisitionThread()
+        {
+            acquisitionWorker.Start();
+        }
 
         /// <summary>
         /// Acquisitor thread logic.
         /// </summary>
 		private void Acquisition_DoWork()
-		{
-            //TO DO: IMPLEMENT
+        {
+            // Implementacija logike periodicnog ocitavanja
+            while (!disposed)
+            {
+                // Ceka signal od tajmera (svake sekunde)
+                acquisitionTrigger.WaitOne();
+
+                // Prolazi kroz sve grupe tacaka definisane u konfiguraciji
+                foreach (var configItem in configuration.GetConfigurationItems())
+                {
+                    // Uvecava brojac sekundi za svaku grupu
+                    configItem.SecondsPassedSinceLastPoll++;
+
+                    // Ako je proslo dovoljno vremena, salje komandu za ocitavanje
+                    if (configItem.SecondsPassedSinceLastPoll >= configItem.AcquisitionInterval)
+                    {
+                        processingManager.ExecuteReadCommand(
+                            configItem,
+                            configuration.GetTransactionId(),
+                            configuration.UnitAddress,
+                            configItem.StartAddress,
+                            configItem.NumberOfRegisters);
+
+                        // Resetuje brojac
+                        configItem.SecondsPassedSinceLastPoll = 0;
+                    }
+                }
+            }
         }
 
         #endregion Private Methods
 
-        /// <inheritdoc />
         public void Dispose()
-		{
-			acquisitionWorker.Abort();
+        {
+            //Postavljamo flag da bi se thread ugasio
+            disposed = true;
         }
-	}
+    }
 }
