ae8a36befac1a9c6a6cd9d852289479eaeae512e
Milos <Milos@DESKTOP-GL6GJFE>
2025-09-20 12:15:43 +0200

    EguConverter, AlarmProcessor

--- ProcessingModule/AlarmProcessor.cs
+++ ProcessingModule/AlarmProcessor.cs
@@ -6,27 +6,42 @@ namespace ProcessingModule
     /// Class containing logic for alarm processing.
     /// </summary>
     public class AlarmProcessor
-	{
+    {
         /// <summary>
         /// Processes the alarm for analog point.
         /// </summary>
-        /// <param name="eguValue">The EGU value of the point.</param>
-        /// <param name="configItem">The configuration item.</param>
+        /// <param name="point">The analog point to check.</param>
         /// <returns>The alarm indication.</returns>
-		public AlarmType GetAlarmForAnalogPoint(double eguValue, IConfigItem configItem)
-		{
-			return AlarmType.NO_ALARM;
-		}
+		public AlarmType GetAlarmForAnalogPoint(IAnalogPoint point)
+        {
+            // IZMENA: Logika za High/Low alarme
+            if (point.EguValue > point.HighLimit)
+            {
+                return AlarmType.HIGH_ALARM;
+            }
+
+            if (point.EguValue < point.LowLimit)
+            {
+                return AlarmType.LOW_ALARM;
+            }
+
+            return AlarmType.NO_ALARM;
+        }
 
         /// <summary>
         /// Processes the alarm for digital point.
         /// </summary>
-        /// <param name="state">The digital point state</param>
-        /// <param name="configItem">The configuration item.</param>
+        /// <param name="point">The digital point to check.</param>
         /// <returns>The alarm indication.</returns>
-		public AlarmType GetAlarmForDigitalPoint(ushort state, IConfigItem configItem)
-		{
+		public AlarmType GetAlarmForDigitalPoint(IDigitalPoint point)
+        {
+            //Logika za Abnormal alarm
+            if (point.RawValue == point.AbnormalValue)
+            {
+                return AlarmType.ABNORMAL_VALUE;
+            }
+
             return AlarmType.NO_ALARM;
         }
-	}
+    }
 }
--- ProcessingModule/EGUConverter.cs
+++ ProcessingModule/EGUConverter.cs
@@ -6,29 +6,39 @@ namespace ProcessingModule
     /// Class containing logic for engineering unit conversion.
     /// </summary>
     public class EGUConverter
-	{
+    {
         /// <summary>
         /// Converts the point value from raw to EGU form.
         /// </summary>
-        /// <param name="scalingFactor">The scaling factor.</param>
-        /// <param name="deviation">The deviation</param>
+        /// <param name="scalingFactor">The scaling factor (A).</param>
+        /// <param name="deviation">The deviation (B).</param>
         /// <param name="rawValue">The raw value.</param>
         /// <returns>The value in engineering units.</returns>
 		public double ConvertToEGU(double scalingFactor, double deviation, ushort rawValue)
-		{
-            return rawValue;
-		}
+        {
+            // IZMENA: Implementacija formule: EGU = A * raw + B
+            return scalingFactor * rawValue + deviation;
+        }
 
         /// <summary>
         /// Converts the point value from EGU to raw form.
         /// </summary>
-        /// <param name="scalingFactor">The scaling factor.</param>
-        /// <param name="deviation">The deviation.</param>
+        /// <param name="scalingFactor">The scaling factor (A).</param>
+        /// <param name="deviation">The deviation (B).</param>
         /// <param name="eguValue">The EGU value.</param>
         /// <returns>The raw value.</returns>
 		public ushort ConvertToRaw(double scalingFactor, double deviation, double eguValue)
         {
-            return (ushort)eguValue;
-		}
-	}
+            //Implementacija formule: raw = (EGU - B) / A
+            if (scalingFactor == 0)
+            {
+                // Izbegavamo deljenje sa nulom, mada se ne bi trebalo desiti.
+                return 0;
+            }
+
+            // Vrednost mora biti pozitivan ceo broj (ushort)
+            double raw = (eguValue - deviation) / scalingFactor;
+            return (ushort)Math.Round(Math.Max(0, raw));
+        }
+    }
 }
