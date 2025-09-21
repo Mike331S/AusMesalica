33475b902f8675dc4c4b1365353d7304e14b3946
Milos <Milos@DESKTOP-GL6GJFE>
2025-09-21 11:49:42 +0200

    ModbusFunctions - ReadHoldingRegistersFunction

--- Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
+++ Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
@@ -24,15 +24,42 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters mrcp = this.CommandParameters as ModbusReadCommandParameters;
+            byte[] request = new byte[12];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.Length)), 0, request, 4, 2);
+            request[6] = mrcp.UnitId;
+            request[7] = mrcp.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.StartAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.Quantity)), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            var retVal = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            ModbusReadCommandParameters mrcp = this.CommandParameters as ModbusReadCommandParameters;
+
+            if (response[7] == (mrcp.FunctionCode + 0x80))
+            {
+                HandeException(response[8]);
+            }
+            else
+            {
+                int byteCount = response[8];
+                int registerCount = byteCount / 2;
+                for (int i = 0; i < registerCount; i++)
+                {
+                    ushort address = (ushort)(mrcp.StartAddress + i);
+                    ushort value = (ushort)IPAddress.NetworkToHostOrder((short)BitConverter.ToUInt16(response, 9 + i * 2));
+                    retVal.Add(new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, address), value);
+                }
+            }
+            return retVal;
         }
     }
 }
