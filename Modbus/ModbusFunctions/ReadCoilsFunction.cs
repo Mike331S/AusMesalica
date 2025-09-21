58ebd304eb6e6c1ce220ed398c5e893872649cbc
Milos <Milos@DESKTOP-GL6GJFE>
2025-09-21 11:26:36 +0200

    ModbusFunctions - ReadCoilsFunction

--- Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -24,15 +24,44 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc/>
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters mrcp = this.CommandParameters as ModbusReadCommandParameters;
+            byte[] request = new byte[12];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.Length)), 0, request, 4, 2);
+            request[6] = mrcp.UnitId;
+            request[7] = mrcp.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.StartAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.Quantity)), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            var retVal = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            ModbusReadCommandParameters mrcp = this.CommandParameters as ModbusReadCommandParameters;
+
+            if (response[7] == (mrcp.FunctionCode + 0x80))
+            {
+                HandeException(response[8]);
+            }
+            else
+            {
+                int byteCount = response[8];
+                for (int i = 0; i < byteCount; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        ushort address = (ushort)(mrcp.StartAddress + i * 8 + j);
+                        ushort value = (ushort)((response[9 + i] >> j) & 1);
+                        retVal.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, address), value);
+                    }
+                }
+            }
+            return retVal;
         }
     }
 }
